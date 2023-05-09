cea683ae113e82d398fb000eda5f2d7d845d6cdf
harysetiawan23 <setiawan230896@gmail.com>
2023-05-09 10:36:32 +0800

    Add  Explicit Account ID

--- environment/staging/main.tf
+++ environment/staging/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  alias = "ap-southeast-1"
+  allowed_account_ids = [ "${var.aws_account_id}" ]
+}
+
 module "webapp" {
   source = "../../module/webapp"
   aws_region = "ap-southeast-1"
--- environment/staging/variable.tf
+++ environment/staging/variable.tf
@@ -1,3 +1,7 @@
 variable "aws_region" {
   description = "AWS Region"
 }
+
+variable "aws_account_id" {
+  description = "AWS Account ID"
+}
